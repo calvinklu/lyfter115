48905343eb8ee4bd0a00fa2f3a31ea94d2f7a21f
gischong <gilbertchong61@gmail.com>
2018-10-21 01:15:08 -0700

    Initial commit

---
+++ app/src/main/java/com/example/gilbert/lyfter115_profile/MyWorkoutsActivity.java
@@ -0,0 +1,14 @@
+package com.example.gilbert.lyfter115_profile;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MyWorkoutsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_workouts);
+        setTitle("My Workouts");
+    }
+}
