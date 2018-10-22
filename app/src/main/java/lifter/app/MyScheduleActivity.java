0b73d083ed23227ec2ae7b216392e34b67a140de
Calvin <calvinklu@yahoo.com>
2018-10-21 20:12:58 -0700

    added stuff for profile page ui's to display

---
+++ app/src/main/java/lifter/app/MyScheduleActivity.java
@@ -0,0 +1,14 @@
+package com.example.gilbert.lyfter115_profile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_schedule);
+        setTitle("My Schedule");
+    }
+}
