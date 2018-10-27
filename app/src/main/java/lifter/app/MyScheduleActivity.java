be1e5117d71bc3d670f2dcf1d21ee4ee4817c314
wyu19 <wyu711@gmail.com>
2018-10-27 15:03:42 -0700

    commit with image of a couch potato as a placeholder for now

--- app/src/main/java/lifter/app/MyScheduleActivity.java
+++ app/src/main/java/lifter/app/MyScheduleActivity.java
@@ -2,13 +2,20 @@ package lifter.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MyScheduleActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_schedule);
         setTitle("My Schedule");
+
+//        ImageView CouchPotato = (ImageView) findViewById(R.id.CouchPotato);
+//        int imageResource = getResources().getIdentifier("@drawable/couchpotato", null, this.getPackageName());
+//        CouchPotato.setImageResource(imageResource);
+
     }
 }
